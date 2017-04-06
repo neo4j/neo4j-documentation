2ec14b37f316962259ba1d228d92777f6398e643
Petra Selmer <petra.selmer@neotechnology.com>
2017-04-06 16:08:14 +0100

    `ORDER BY`: port to DocumentingTest & page-level TOC

--- cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/OrderByTest.scala
+++ cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/OrderByTest.scala
@@ -19,60 +19,99 @@
  */
 package org.neo4j.cypher.docgen
 
-import org.junit.Assert._
-import org.junit.Test
-import org.neo4j.graphdb.Node
-import org.neo4j.visualization.graphviz.{AsciiDocSimpleStyle, GraphStyle}
+import org.neo4j.cypher.docgen.tooling.{DocBuilder, DocumentingTest, ResultAssertions}
 
-class OrderByTest extends DocumentingTestBase with SoftReset {
-  override def graphDescription = List("A KNOWS B", "B KNOWS C")
+class OrderByTest extends DocumentingTest {
+  override def outputPath = "target/docs/dev/ql"
 
-  override protected def getGraphvizStyle: GraphStyle =
-    AsciiDocSimpleStyle.withAutomaticRelationshipTypeColors()
-
-  override val properties = Map(
-    "A" -> Map("age" -> 34, "length"->170),
-    "B" -> Map("age" -> 34),
-    "C" -> Map("age" -> 32, "length"->185))
-
-  def section = "Order by"
-
-  @Test def sortByName() {
-    testQuery(
-      title = "Order nodes by property",
-      text = "`ORDER BY` is used to sort the output.",
-      queryText = """MATCH (n) RETURN n ORDER BY n.name""",
-      optionalResultExplanation = """The nodes are returned, sorted by their name.""",
-      assertions = (p) => assertEquals(List(node("A"), node("B"), node("C")), p.columnAs[Node]("n").toList))
-  }
-
-  @Test def sortByNameReverse() {
-    testQuery(
-      title = "Order nodes in descending order",
-      text = "By adding `DESC[ENDING]` after the variable to sort on, the sort will be done in reverse order.",
-      queryText = """MATCH (n) RETURN n ORDER BY n.name DESC""",
-      optionalResultExplanation = """The example returns the nodes, sorted by their name in reverse order.""",
-      assertions = (p) => assertEquals(List(node("C"), node("B"), node("A")), p.columnAs[Node]("n").toList))
-  }
-
-  @Test def sortByMultipleColumns() {
-    testQuery(
-      title = "Order nodes by multiple properties",
-      text = "You can order by multiple properties by stating each variable in the `ORDER BY`" +
-        " clause. Cypher will sort the result by the first variable listed, and for equals values, " +
-        "go to the next property in the `ORDER BY` clause, and so on.",
-      queryText = """MATCH (n) RETURN n ORDER BY n.age, n.name""",
-      optionalResultExplanation = """This returns the nodes, sorted first by their age, and then by their name.""",
-      assertions = (p) => assertEquals(List(node("C"), node("A"), node("B")), p.columnAs[Node]("n").toList))
-  }
-
-  @Test def order_by_nullable_property() {
-    testQuery(
-      title = "Ordering null",
-      text = "When sorting the result set, `null` will always come at the end of the result set for" +
-        " ascending sorting, and first when doing descending sort.",
-      queryText = """MATCH (n) RETURN n.length, n ORDER BY n.length""",
-      optionalResultExplanation = """The nodes are returned sorted by the length property, with a node without that property last.""",
-      assertions = (p) => assertEquals(List(node("A"), node("C"), node("B")), p.columnAs[Node]("n").toList))
-  }
+  override def doc = new DocBuilder {
+    doc("ORDER BY", "query-order")
+    initQueries(
+      """
+        |CREATE (a {name: 'A', age: 34, length: 170}),
+        |       (b {name: 'B', age: 34}),
+        |       (c {name: 'C', age: 32, length: 185}),
+        |
+        |       (a)-[:KNOWS]->(b),
+        |       (b)-[:KNOWS]->(c)
+      """.stripMargin
+    )
+    synopsis("`ORDER BY` is a sub-clause following `RETURN` or `WITH`, and it specifies that the output should be sorted and how.")
+    p(
+      """* <<order-introduction, Introduction>>
+        |* <<order-nodes-by-property, Order nodes by property>>
+        |* <<order-nodes-by-multiple-properties, Order nodes by multiple properties>>
+        |* <<order-nodes-in-descending-order, Order nodes in descending order>>
+        |* <<order-null, Ordering `null`>>
+      """.stripMargin)
+    section("Introduction", "order-introduction") {
+      p(
+        """Note that you cannot sort on nodes or relationships, just on properties on these.
+          |`ORDER BY` relies on comparisons to sort the output, see <<cypher-ordering>>.""".stripMargin)
+      p(
+        """In terms of scope of variables, `ORDER BY` follows special rules, depending on if the projecting `RETURN` or `WITH` clause is either aggregating or `DISTINCT`.
+           If it is an aggregating or `DISTINCT` projection, only the variables available in the projection are available.
+           If the projection does not alter the output cardinality (which aggregation and `DISTINCT` do), variables available from before the projecting clause are also available.
+           When the projection clause shadows already existing variables, only the new variables are available.
+        """.stripMargin)
+      p(
+        """Lastly, it is not allowed to use aggregating expressions in the `ORDER BY` sub-clause if they are not also listed in the projecting clause.
+           This last rule is to make sure that `ORDER BY` does not change the results, only the order of them.
+        """.stripMargin)
+      graphViz()
+    }
+    section("Order nodes by property", "order-nodes-by-property") {
+      p(
+        """`ORDER BY` is used to sort the output.""".stripMargin)
+      query(
+        """MATCH (n)
+          |RETURN n.name, n.age
+          |ORDER BY n.name""".stripMargin, ResultAssertions((r) => {
+        r.toList should equal(List(Map("n.name" -> "A", "n.age" -> 34), Map("n.name" -> "B", "n.age" -> 34), Map("n.name" -> "C", "n.age" -> 32)))
+      })) {
+        p("The nodes are returned, sorted by their name.")
+        resultTable()
+      }
+    }
+    section("Order nodes by multiple properties", "order-nodes-by-multiple-properties") {
+      p(
+        """You can order by multiple properties by stating each variable in the `ORDER BY` clause.
+          |Cypher will sort the result by the first variable listed, and for equals values, go to the next property in the `ORDER BY` clause, and so on.""".stripMargin)
+      query(
+        """MATCH (n)
+          |RETURN n.name, n.age
+          |ORDER BY n.age, n.name""".stripMargin, ResultAssertions((r) => {
+          r.toList should equal(List(Map("n.age" -> 32, "n.name" -> "C"), Map("n.age" -> 34, "n.name" -> "A"), Map("n.age" -> 34, "n.name" -> "B")))
+        })) {
+        p("This returns the nodes, sorted first by their age, and then by their name.")
+        resultTable()
+      }
+    }
+    section("Order nodes in descending order", "order-nodes-in-descending-order") {
+      p(
+        """By adding `DESC[ENDING]` after the variable to sort on, the sort will be done in reverse order.""".stripMargin)
+      query(
+        """MATCH (n)
+          |RETURN n.name, n.age
+          |ORDER BY n.name DESC""".stripMargin, ResultAssertions((r) => {
+          r.toList should equal(List(Map("n.age" -> 32, "n.name" -> "C"), Map("n.age" -> 34, "n.name" -> "B"), Map("n.age" -> 34, "n.name" -> "A")))
+        })) {
+        p("The example returns the nodes, sorted by their name in reverse order.")
+        resultTable()
+      }
+    }
+    section("Ordering `null`", "order-null") {
+      p(
+        """When sorting the result set, `null` will always come at the end of the result set for ascending sorting, and first when doing descending sort.""".stripMargin)
+      query(
+        """MATCH (n)
+          |RETURN n.length, n.name, n.age
+          |ORDER BY n.length""".stripMargin, ResultAssertions((r) => {
+          r.toList should equal(List(Map("n.age" -> 34, "n.name" -> "A", "n.length" -> 170), Map("n.age" -> 32, "n.name" -> "C", "n.length" -> 185), Map("n.age" -> 34, "n.name" -> "B", "n.length" -> null)))
+        })) {
+        p("The nodes are returned sorted by the length property, with a node without that property last.")
+        resultTable()
+      }
+    }
+  }.build()
 }
