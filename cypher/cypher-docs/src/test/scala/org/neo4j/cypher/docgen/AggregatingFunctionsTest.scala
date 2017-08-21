2b31c550bf626ce7a904d8dba2bddf354f9e8dcf
Petra Selmer <petra.selmer@neotechnology.com>
2017-08-21 14:21:10 +0100

    Fixed and improved content on aggregating functions

--- cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/AggregatingFunctionsTest.scala
+++ cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/AggregatingFunctionsTest.scala
@@ -84,44 +84,50 @@ class AggregatingFunctionsTest extends DocumentingTest {
     p("The following graph is used for the examples below:")
     graphViz()
     section("avg()", "functions-avg") {
-      p("`avg()` calculates the average of a numeric column.")
+      p(
+        "`avg()` returns the average value of a numeric expression `expr`.")
       function("avg(expression)", ("expression", "A numeric expression."))
+      considerations("Any `null` values are excluded from the calculation.", "`avg(null)` returns `null`.")
       query("MATCH (n:Person) RETURN avg(n.age)", ResultAssertions((r) => {
         r.toList.head("avg(n.age)") should equal(30L)
       })) {
-        p("The average of all the values in the property `age` is returned by the example query.")
+        p("The query returns the average of all the values in the property `age`.")
         resultTable()
       }
     }
     section("collect()", "functions-collect") {
       p(
-        """`collect()` collects all the values into a list.
-          |It will ignore `null` values.""".stripMargin)
+        """`collect()` returns a list containing the values returned by an expression `expr`.
+          |Using this function aggregates data by amalgamating multiple records or values into a single list.""".stripMargin)
       function("collect(expression)", ("expression", "An expression."))
+      considerations("Any `null` values are ignored and will not be added to the list.", "`collect(null)` returns an empty list.")
       query("MATCH (n:Person) RETURN collect(n.age)", ResultAssertions((r) => {
         r.toList.head("collect(n.age)") should equal(Seq(13, 33, 44))
       })) {
-        p("Returns a single row, with all the values collected.")
+        p("All the values are collected and returned in a single list.")
         resultTable()
       }
     }
     section("count()", "functions-count") {
       p(
-        """`count()` is used to count the number of rows.
-          |`count()` can be used in two forms -- `count(*)` which just counts the number of matching rows, and `count(<expression>)`, which counts the number of non-`null` values in `<expression>`.
+        """`count()` returns the number of values or rows, and appears in two variants:
+          |
+          |* `count(*)` returns the number of matching rows, and
+          |* `count(expr)` returns the number of non-`null` values returned by an expression `expr`.
         """.stripMargin)
       function("count(expression)", ("expression", "An expression."))
-      section("Count nodes") {
-        p("`count(*)` can be used to count the number of nodes; for example, the number of nodes connected to one node.")
+      considerations("`count(*)` includes rows returning `null`.", "`count(expr)` ignores `null` values.", "`count(null)` returns `0`.")
+      section("Using `count(*)` to return the number of nodes") {
+        p("`count(*)` can be used to return the number of nodes; for example, the number of nodes connected to some node `n`.")
         query("MATCH (n {name: 'A'})-->(x) RETURN labels(n), n.age, count(*)", ResultAssertions((r) => {
           r.toList should equal(List(Map("labels(n)" -> List("Person"), "n.age" -> 13L, "count(*)" -> 3L)))
         })) {
-          p("This returns the start node and the count of related nodes.")
+          p("This returns the labels and `age` property of the start node `n` and the number of nodes related to `n`.")
           resultTable()
         }
       }
-      section("Group count relationship types") {
-        p("To count the groups of relationship types, return the types and count them with `count(*)`.")
+      section("Using `count(*)` to group and count relationship types") {
+        p("`count(*)` can be used to group relationship types and return the number of each of these.")
         query("MATCH (n {name: 'A'})-[r]->() RETURN type(r), count(*)", ResultAssertions((r) => {
           r.toList should equal(List(Map("type(r)" -> "KNOWS", "count(*)" -> 3L)))
         })) {
@@ -129,8 +135,8 @@ class AggregatingFunctionsTest extends DocumentingTest {
           resultTable()
         }
       }
-      section("Count entities") {
-        p("Instead of counting the number of results with `count(*)`, it might be more expressive to include the name of the variable you care about.")
+      section("Using `count(expression)` to return the number of values") {
+        p("Instead of simply returning the number of rows with `count(*)`, it may be more useful to return the actual number of values returned by an expression.")
         query("MATCH (n {name: 'A'})-->(x) RETURN count(x)", ResultAssertions((r) => {
           r.toList should equal(List(Map("count(x)" -> 3L)))
         })) {
@@ -138,34 +144,36 @@ class AggregatingFunctionsTest extends DocumentingTest {
           resultTable()
         }
       }
-      section("Count non-`null` values") {
-        p("The non-`null` values can be counted by using `count(<expression>)`.")
+      section("Counting non-`null` values") {
+        p("`count(expression)` can be used to return the number of non-`null` values returned by the expression.")
         query("MATCH (n:Person) RETURN count(n.age)", ResultAssertions((r) => {
           r.toList should equal(List(Map("count(n.age)" -> 3L)))
         })) {
-          p("The count of related nodes with the `age` property set is returned.")
+          p("The number of `:Person` nodes having an `age` property is returned.")
           resultTable()
         }
       }
       section("Counting with and without duplicates") {
         p(
-          """In this example we are trying to find all our friends of friends, and count them.
-            |The first aggregate function, `count(DISTINCT friend_of_friend)`, will only see a `friend_of_friend` once -- `DISTINCT` removes the duplicates.
-            |The latter aggregate function, `count(friend_of_friend)`, might very well see the same `friend_of_friend` multiple times.
-            |In this case, both `B` and `C` know `D` and thus `D` will get counted twice, when not using `DISTINCT`.""")
+          """In this example we are trying to find all our friends of friends, and count them:
+            |
+            |* The first aggregate function, `count(DISTINCT friend_of_friend)`, will only count a `friend_of_friend` once, as `DISTINCT` removes the duplicates.
+            |* The second aggregate function, `count(friend_of_friend)`, will consider the same `friend_of_friend` multiple times.""")
         query(
           """MATCH (me:Person)-->(friend:Person)-->(friend_of_friend:Person)
             |WHERE me.name = 'A'
             |RETURN count(DISTINCT friend_of_friend), count(friend_of_friend)""".stripMargin, ResultAssertions((r) => {
             r.toList should equal(List(Map("count(DISTINCT friend_of_friend)" -> 1L, "count(friend_of_friend)" -> 2L)))
           })) {
+          p("Both `B` and `C` know `D` and thus `D` will get counted twice when not using `DISTINCT`.")
           resultTable()
         }
       }
     }
     section("max()", "functions-max") {
-      p("`max()` takes a numeric property as input, and returns the highest value in that column.")
-      function("max(expression)", ("expression", "A numeric expression."))
+      p("`max()` returns the maximum value in a set of values returned by an expression `expr`.")
+      function("max(expression)", ("expression", "A numeric or string expression."))
+      considerations("Any `null` values are excluded from the calculation.", "`max(null)` returns `null`.")
       query("MATCH (n:Person) RETURN max(n.age)", ResultAssertions((r) => {
         r.toList.head("max(n.age)") should equal(44L)
       })) {
@@ -174,8 +182,9 @@ class AggregatingFunctionsTest extends DocumentingTest {
       }
     }
     section("min()", "functions-min") {
-      p("`min()` takes a numeric property as input, and returns the lowest value in that column.")
-      function("min(expression)", ("expression", "A numeric expression."))
+      p("`min()` returns the maximum value in a set of values returned by an expression `expr`.")
+      function("min(expression)", ("expression", "A numeric or string expression."))
+      considerations("Any `null` values are excluded from the calculation.", "`min(null)` returns `null`.")
       query("MATCH (n:Person) RETURN min(n.age)", ResultAssertions((r) => {
         r.toList.head("min(n.age)") should equal(13L)
       })) {
@@ -185,36 +194,39 @@ class AggregatingFunctionsTest extends DocumentingTest {
     }
     section("percentileCont()", "functions-percentilecont") {
       p(
-        """`percentileCont()` calculates the percentile of a given value over a group, with a percentile from 0.0 to 1.0.
-          |It uses a linear interpolation method, calculating a weighted average between two values, if the desired percentile lies between them.
+        """`percentileCont()` returns the percentile of a given value over a group, with a percentile from 0.0 to 1.0.
+          |It uses a linear interpolation method, calculating a weighted average between two values if the desired percentile lies between them.
           |For nearest values using a rounding method, see `percentileDisc`.""")
       function("percentileCont(expression, percentile)", ("expression", "A numeric expression."), ("percentile", "A numeric value between 0.0 and 1.0"))
+      considerations("Any `null` values are excluded from the calculation.", "`percentileCont(null, <percentile>)` returns `null`.")
       query("MATCH (n:Person) RETURN percentileCont(n.age, 0.4)", ResultAssertions((r) => {
         r.toList.head("percentileCont(n.age, 0.4)") should equal(29L)
       })) {
-        p("The 40th percentile of the values in the property `age` is returned by the example query, calculated with a weighted average.")
+        p("The 40th percentile of the values in the property `age` is returned, calculated with a weighted average.")
         resultTable()
       }
     }
     section("percentileDisc()", "functions-percentiledisc") {
       p(
-        """`percentileDisc()` calculates the percentile of a given value over a group, with a percentile from 0.0 to 1.0.
-          |It uses a rounding method, returning the nearest value to the percentile.
+        """`percentileDisc()` returns the percentile of a given value over a group, with a percentile from 0.0 to 1.0.
+          |It uses a rounding method and calculates the nearest value to the percentile.
           |For interpolated values, see `percentileCont`.""")
       function("percentileDisc(expression, percentile)", ("expression", "A numeric expression."), ("percentile", "A numeric value between 0.0 and 1.0"))
+      considerations("Any `null` values are excluded from the calculation.", "`percentileDisc(null, <percentile>)` returns `null`.")
       query("MATCH (n:Person) RETURN percentileDisc(n.age, 0.5)", ResultAssertions((r) => {
         r.toList.head("percentileDisc(n.age, 0.5)") should equal(33L)
       })) {
-        p("The 50th percentile of the values in the property `age` is returned by the example query. In this case, 0.5 is the median, or 50th percentile.")
+        p("The 50th percentile of the values in the property `age` is returned. In this case, 0.5 is the median, or 50th percentile.")
         resultTable()
       }
     }
     section("stDev()", "functions-stdev") {
       p(
-        """`stDev()` calculates the standard deviation for a given value over a group.
+        """`stDev()` returns the standard deviation for a given value over a group.
           |It uses a standard two-pass method, with `N - 1` as the denominator, and should be used when taking a sample of the population for an unbiased estimate.
           |When the standard variation of the entire population is being calculated, `stdDevP` should be used.""")
       function("stDev(expression)", ("expression", "A numeric expression."))
+      considerations("Any `null` values are excluded from the calculation.", "`stDev(null)` returns `0`.")
       query("MATCH (n) WHERE n.name IN ['A', 'B', 'C'] RETURN stDev(n.age)", ResultAssertions(f = (r) => {
         assertEquals(15.7162336455, r.toList.head("stDev(n.age)").asInstanceOf[Double], 0.0000001)
       })) {
@@ -224,26 +236,26 @@ class AggregatingFunctionsTest extends DocumentingTest {
     }
     section("stDevP()", "functions-stdevp") {
       p(
-        """`stDevP()` calculates the standard deviation for a given value over a group.
+        """`stDevP()` returns the standard deviation for a given value over a group.
           |It uses a standard two-pass method, with `N` as the denominator, and should be used when calculating the standard deviation for an entire population.
           |When the standard variation of only a sample of the population is being calculated, `stDev` should be used.""")
       function("stDevP(expression)", ("expression", "A numeric expression."))
+      considerations("Any `null` values are excluded from the calculation.", "`stDevP(null)` returns `0`.")
       query("MATCH (n) WHERE n.name IN ['A', 'B', 'C'] RETURN stDevP(n.age)", ResultAssertions((r) => {
         assertEquals(12.8322510366, r.toList.head("stDevP(n.age)").asInstanceOf[Double], 0.0000001)
       })) {
-        p("The population standard deviation of the values in the property `age` is returned by the example query.")
+        p("The population standard deviation of the values in the property `age` is returned.")
         resultTable()
       }
     }
     section("sum()", "functions-sum") {
-      p(
-        """`sum()` simply adds up all the numeric values it encounters.
-          |Any `null` values are silently dropped.""".stripMargin)
+      p("`sum()` returns the sum of all the non-`null` values returned by a numeric expression `expr`.")
       function("sum(expression)", ("expression", "A numeric expression."))
+      considerations("Any `null` values are excluded from the calculation.", "`sum(null)` returns `0`.")
       query("MATCH (n:Person) RETURN sum(n.age)", ResultAssertions((r) => {
         r.toList.head("sum(n.age)") should equal(90L)
       })) {
-        p("This returns the sum of all the values in the property `age`.")
+        p("The sum of all the values in the property `age` is returned.")
         resultTable()
       }
     }
