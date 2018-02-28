2b0020389257bc4a0afed0bf806b5c204ff48117
Jonatan Jäderberg <jonatan.jaderberg@neotechnology.com>
2018-02-28 11:50:41 +0100

    Fix sloppy fw merge

--- cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/tooling/Document.scala
+++ cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/tooling/Document.scala
@@ -25,7 +25,7 @@ import org.neo4j.cypher.internal.compiler.v3_2.prettifier.Prettifier
 import org.neo4j.cypher.internal.frontend.v3_2.InternalException
 import org.neo4j.cypher.docgen.tooling.RunnableInitialization.InitializationFunction
 import org.neo4j.cypher.javacompat.internal.GraphDatabaseCypherService
-import org.neo4j.cypher.internal.frontend.v3_1.helpers.Eagerly
+import org.neo4j.cypher.internal.frontend.v3_2.helpers.Eagerly
 import org.neo4j.kernel.GraphDatabaseQueryService
 import scala.collection.JavaConverters._
 
