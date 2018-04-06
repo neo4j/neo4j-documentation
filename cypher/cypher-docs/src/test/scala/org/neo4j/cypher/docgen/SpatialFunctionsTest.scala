637c2a365a64064365c2c605dad9d9d0039a547a
Petra Selmer <petra.selmer@neotechnology.com>
2018-04-06 15:47:25 +0100

    cartesian => Cartesian

--- cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/SpatialFunctionsTest.scala
+++ cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/SpatialFunctionsTest.scala
@@ -62,7 +62,7 @@ class SpatialFunctionsTest extends DocumentingTest {
         |
         |       (copenhagen)-[:TRAVEL_ROUTE]->(malmo)""")
     synopsis("These functions are used to specify points in a 2D coordinate system and to calculate the geodesic distance between two points.")
-    p("Two coordinate reference systems (CRS) are supported: (i) http://spatialreference.org/ref/epsg/4326/[WGS 84] and (ii) http://spatialreference.org/ref/sr-org/7203/[cartesian 2D].")
+    p("Two coordinate reference systems (CRS) are supported: (i) http://spatialreference.org/ref/epsg/4326/[WGS 84] and (ii) http://spatialreference.org/ref/sr-org/7203/[Cartesian 2D].")
     p("""_WGS 84_ is specified with a map containing coordinate values for either `longitude` and `latitude` (this is the default), or `x` and `y`.
         |_Cartesian_ is specified with a map containing only `x` and `y` coordinate values.""" stripMargin)
     p(
@@ -70,22 +70,22 @@ class SpatialFunctionsTest extends DocumentingTest {
         |
         |* <<functions-distance,distance()>>
         |* <<functions-point,point() - WGS 84>>
-        |* <<functions-point-cartesian,point() - cartesian 2D>>
+        |* <<functions-point-cartesian,point() - Cartesian 2D>>
       """.stripMargin)
     p("The following graph is used for some of the examples below.")
     graphViz()
     section("distance()", "functions-distance") {
       p(
         """`distance()` returns a floating point number representing the geodesic distance between two points in the same CRS.
-          |If the points are in the _cartesian_ CRS, then the units of the returned distance will be the same as the units of the points, calculated using Pythagoras' theorem.
+          |If the points are in the _Cartesian_ CRS, then the units of the returned distance will be the same as the units of the points, calculated using Pythagoras' theorem.
           |If the points are in the _WGS-84_ CRS, then the units of the returned distance will be meters, based on the haversine formula over a spherical earth approximation.
         """.stripMargin)
-      function("distance(point1, point2)", "A Float.", ("point1", "A point in either the WGS 84 or cartesian CRS."), ("point2", "A point in the same CRS as 'point1'."))
+      function("distance(point1, point2)", "A Float.", ("point1", "A point in either the WGS 84 or Cartesian CRS."), ("point2", "A point in the same CRS as 'point1'."))
       considerations("`distance(null, null)`, `distance(null, point2)` and `distance(point1, null)` all return `null`.")
       query("WITH point({x: 2.3, y: 4.5, crs: 'cartesian'}) as p1, point({x: 1.1, y: 5.4, crs: 'cartesian'}) as p2\nRETURN distance(p1,p2) AS dist", ResultAssertions((r) => {
         r.toList.head("dist").asInstanceOf[Double] should equal(1.5)
       })) {
-        p("The distance between two points in the _cartesian_ CRS is returned.")
+        p("The distance between two points in the _Cartesian_ CRS is returned.")
         resultTable()
       }
       query(
@@ -133,14 +133,14 @@ class SpatialFunctionsTest extends DocumentingTest {
         resultTable()
       }
     }
-    section("point() - cartesian 2D", "functions-point-cartesian") {
-      p("`point()` returns a point in the _cartesian_ coordinate system corresponding to the given coordinate values.")
+    section("point() - Cartesian 2D", "functions-point-cartesian") {
+      p("`point()` returns a point in the _Cartesian_ coordinate system corresponding to the given coordinate values.")
       function("point({x, y [, crs]})", "A Point.", ("A single map consisting of the following:", ""), ("x", "A numeric expression"), ("y", "A numeric expression"), ("crs", "The string 'cartesian'"))
       considerations("If any argument provided to `point()` is `null`, `null` will be returned.")
       query("RETURN point({x: 2.3, y: 4.5}) AS point", ResultAssertions((r) => {
         r.toList should equal(List(Map("point" -> CartesianPoint(2.3, 4.5, CRS.Cartesian))))
       })) {
-        p("A point with an `x` coordinate of `2.3` and a `y` coordinate of `4.5` in the _cartesian_ CRS is returned")
+        p("A point with an `x` coordinate of `2.3` and a `y` coordinate of `4.5` in the _Cartesian_ CRS is returned")
         resultTable()
       }
     }
