4781f82e66a2c6ed5a0d5d497402f5121a0d43d9
Chris Gioran <chris@neotechnology.com>
2018-05-14 13:10:50 +0200

    Updates license and copyright
    Keeping in sync with changes in neo4j repo, update
     copyright notices and AGPL license text to new
     specs.

--- cypher/cypher-docs/src/test/scala/org/neo4j/cypher/internal/compatibility/v3_4/runtime/CypherSerializer.scala
+++ cypher/cypher-docs/src/test/scala/org/neo4j/cypher/internal/compatibility/v3_4/runtime/CypherSerializer.scala
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2002-2018 "Neo Technology,"
- * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ * Copyright (c) 2002-2018 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
  *
  * This file is part of Neo4j.
  *
@@ -19,25 +19,6 @@
  */
 package org.neo4j.cypher.internal.compatibility.v3_4.runtime
 
-/*
- * Copyright (c) 2002-2017 "Neo Technology,"
- * Network Engine for Objects in Lund AB [http://neotechnology.com]
- *
- * This file is part of Neo4j.
- *
- * Neo4j is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 import org.neo4j.cypher.internal.runtime.QueryContext
 import org.neo4j.cypher.internal.runtime.interpreted.IsList
 import org.neo4j.cypher.internal.runtime.interpreted.commands.values.KeyToken
