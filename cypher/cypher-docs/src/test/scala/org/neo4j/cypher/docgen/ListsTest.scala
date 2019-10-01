b408a83e09d24baf4cd6c529e2bfe3767fb24ae3
David Oliver <davidoliverSP2@users.noreply.github.com>
2019-10-01 10:13:05 +0100

    use the correct casing on orderby

--- cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/ListsTest.scala
+++ cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/ListsTest.scala
@@ -164,7 +164,7 @@ class ListsTest extends DocumentingTest {
       graphViz()
       query(
         """MATCH (a:Person {name: 'Keanu Reeves'})
-          |RETURN [(a)-->(b) WHERE b:Movie | b.released] AS Years ORDER BY YEARS ASC""", ResultAssertions((r) => {
+          |RETURN [(a)-->(b) WHERE b:Movie | b.released] AS Years ORDER BY Years ASC""", ResultAssertions((r) => {
           r.toList should equal(List(Map("Years" -> List(1995, 1997, 1999, 2000, 2003, 2003, 2003))))
         })) {
         resultTable()
