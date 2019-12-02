ebd0f39c08f3b6495fe05e6c0679c839825175fe
AlexicaWright <jessicawright@neotechnology.com>
2019-12-02 10:27:52 +0100

    update

--- cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/DatabasesTest.scala
+++ cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/DatabasesTest.scala
@@ -46,7 +46,7 @@ class DatabasesTest extends DocumentingTest with QueryStatisticsTestSupport {
       note {
         p("Note that for failed databases, the `currentStatus` and `requestedStatus` are different. " +
         "This often implies an error, but **does not always**. " +
-        "For example, a database may take a while to transition from "offline" to "online" due to performing recovery. " +
+        "For example, a database may take a while to transition from `offline` to `online` due to performing recovery. " +
         "Or, during normal operation a database's `currentStatus` may be transiently different from its `requestedStatus` due to a necessary automatic process, such as one Neo4j instance copying store files from another. " +
         "The possible statuses are `initial`, `online`, `offline`, `store copying` and `unknown`.")
       }
