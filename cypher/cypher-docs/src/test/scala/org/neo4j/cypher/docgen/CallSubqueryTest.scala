57b9575b3fe06e2e599095391209b3e52b5b1908
Georgiy Kargapolov <inmost-light@users.noreply.github.com>
2020-02-04 10:32:49 +0100

    Update subqueries docs

--- cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/CallSubqueryTest.scala
+++ cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/CallSubqueryTest.scala
@@ -15,11 +15,13 @@ class CallSubqueryTest extends DocumentingTest {
       |CREATE (a)-[:CHILD_OF]->(c)
     """.stripMargin)
     synopsis("The `CALL {}` clause evaluates a subquery that returns some values.")
+
     p(
       """* <<subquery-call-introduction, Introduction>>
-        |* <<subquery-post-union, Post- union processing>>
+        |* <<subquery-correlated-importing, Importing variables into subqueries>>
+        |* <<subquery-post-union, Post-union processing>>
         |* <<subquery-aggregation, Aggregation and side-effects>>
-        |* <<subquery-correlated, Correlated subqueries>>
+        |* <<subquery-correlated-aggregation, Aggregation on imported variables>>
         |"""
       )
 
@@ -40,12 +42,37 @@ class CallSubqueryTest extends DocumentingTest {
       p( """There are restrictions on what queries are allowed as subqueries and how they interact with the enclosing query:""")
       p( """
            |* A subquery must end with a `RETURN` clause.
-           |* A subquery cannot refer to variables from the enclosing query.
+           |* A subquery can only refer to variables from the enclosing query if they are explicitly imported.
            |* A subquery cannot return variables with the same names as variables in the enclosing query.
            |* All variables that are returned from a subquery are afterwards available in the enclosing query.
          """.stripMargin)
       p("The following graph is used for the examples below:")
       graphViz()
+    }
+
+    section( "Importing variables into subqueries", "subquery-correlated-importing") {
+      p("""Variables are imported into a subquery using an importing `WITH` clause.
+          |As the subquery is evaluated for each incoming input row, the imported variables get bound to the corresponding values from the input row in each evaluation.
+          |""".stripMargin)
+
+      preformattedQuery("""UNWIND [0, 1, 2] AS x
+                          |CALL {
+                          |  WITH x
+                          |  RETURN x * 10 AS y
+                          |}
+                          |RETURN x, y
+                          |""".stripMargin,
+        ResultAssertions(r => r.toSet should equal(Set(
+          Map("x" -> 0, "y" -> 0),
+          Map("x" -> 1, "y" -> 10),
+          Map("x" -> 2, "y" -> 20),
+        )))) { resultTable() }
+
+      p("An importing `WITH` clause must:")
+      p("""
+          |* Consist only of simple references to outside variables - e.g. `WITH x, y, z`. Aliasing or expressions are not supported in importing `WITH` clauses - e.g. `WITH a AS b` or `WITH a+1 AS b`.
+          |* Be the first clause of a subquery (or the second clause, if directly following a `USE` clause).
+          |""".stripMargin)
     }
 
     section("Post-union processing", "subquery-post-union") {
@@ -58,25 +85,29 @@ class CallSubqueryTest extends DocumentingTest {
                            |}
                            |RETURN p.name, p.age ORDER BY p.name
                          """.stripMargin,
-                         ResultAssertions(r => r.toList should equal(Seq(Map("p.name" -> "Alice", "p.age" -> 20),
-                                                                         Map("p.name" -> "Charlie", "p.age" -> 65)
-                                                                         )))) {
-        resultTable()
-      }
+        ResultAssertions(r => r.toList should equal(Seq(
+          Map("p.name" -> "Alice", "p.age" -> 20),
+          Map("p.name" -> "Charlie", "p.age" -> 65)
+        )))) { resultTable() }
+
       p( """If different parts of a result should be matched differently, with some aggregation over the whole results, subqueries need to be used.
-        |This example query finds all persons with friends in one part of the union and all children with parents in the other part.
-        |Subsequently the number of friends and parents is counted together.
+        |This example query finds friends and/or parents for each person.
+        |Subsequently the number of friends and parents are counted together.
       """.stripMargin)
-      preformattedQuery( """CALL {
-                           |  MATCH (p:Person)-[:FRIEND_OF]->(other:Person) RETURN p, other
+      preformattedQuery( """MATCH (p:Person)
+                           | CALL {
+                           |  WITH p OPTIONAL MATCH (p)-[:FRIEND_OF]->(other:Person) RETURN other
                            |  UNION
-                           |  MATCH (p:Child)-[:CHILD_OF]->(other:Parent) RETURN p, other
+                           |  WITH p OPTIONAL MATCH (p)-[:CHILD_OF]->(other:Parent) RETURN other
                            |}
                            |RETURN DISTINCT p.name, count(other)
                          """.stripMargin,
-                         ResultAssertions(r => r.toList should equal(Seq(Map("p.name" -> "Alice", "count(other)" -> 2))))) {
-        resultTable()
-      }
+        ResultAssertions(r => r.toSet should equal(Set(
+          Map("p.name" -> "Alice", "count(other)" -> 2),
+          Map("p.name" -> "Bob", "count(other)" -> 0),
+          Map("p.name" -> "Charlie", "count(other)" -> 0),
+          Map("p.name" -> "Dora", "count(other)" -> 0),
+        )))) { resultTable() }
     }
 
     section("Aggregation and side-effects", "subquery-aggregation") {
@@ -92,16 +123,32 @@ class CallSubqueryTest extends DocumentingTest {
                            |  RETURN count(c) AS numberOfClones
                            |}
                            |RETURN p.name, numberOfClones
-                         """.stripMargin,
-                         ResultAssertions(r => ()/*r.toList should equal(Seq(Map("p.name" -> "Alice", "numberOfClones" -> 5),
-                                                                         Map("p.name" -> "Bob", "numberOfClones" -> 5),
-                                                                         Map("p.name" -> "Charlie", "numberOfClones" -> 5),
-                                                                         Map("p.name" -> "Dora", "numberOfClones" -> 5)
-                                                                         ))*/)) {
-        resultTable()
-      }
+                           |""".stripMargin,
+        ResultAssertions(r => r.toSet should equal(Set(
+          Map("p.name" -> "Alice", "numberOfClones" -> 5),
+          Map("p.name" -> "Bob", "numberOfClones" -> 5),
+          Map("p.name" -> "Charlie", "numberOfClones" -> 5),
+          Map("p.name" -> "Dora", "numberOfClones" -> 5)
+        )))) { resultTable() }
     }
 
+    section("Aggregation on imported variables", "subquery-correlated-aggregation") {
+      p("Aggregations in subqueries are scoped to the subquery evaluation, also for imported variables. The following example counts the number of younger persons for each person in the graph:")
+        preformattedQuery("""MATCH (p:Person)
+                            |CALL {
+                            |  WITH p
+                            |  MATCH (other:Person) WHERE other.age < p.age
+                            |  RETURN count(other) AS youngerPersonsCount
+                            |}
+                            |RETURN p.name, youngerPersonsCount
+                            |""".stripMargin,
+          ResultAssertions(r => r.toSet should equal(Set(
+            Map("p.name" -> "Alice", "youngerPersonsCount" -> 0),
+            Map("p.name" -> "Bob", "youngerPersonsCount" -> 1),
+            Map("p.name" -> "Charlie", "youngerPersonsCount" -> 3),
+            Map("p.name" -> "Dora", "youngerPersonsCount" -> 2)
+          )))) { resultTable() }
+  }
   }.build()
 }
 
