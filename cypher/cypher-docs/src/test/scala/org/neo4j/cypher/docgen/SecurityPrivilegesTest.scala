445c3b65c5f475492fce1ff87f7e56ff0f741275
Therese Magnusson <scout.therese@gmail.com>
2020-11-12 09:52:35 +0100

    Update graph image include

--- cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/SecurityPrivilegesTest.scala
+++ cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/SecurityPrivilegesTest.scala
@@ -56,9 +56,8 @@ class SecurityPrivilegesTest extends DocumentingTest with QueryStatisticsTestSup
       p("include::grant-deny-syntax.asciidoc[]")
       p("image::grant-privileges-overview.png[title=\"GRANT and DENY Syntax\"]")
       // image source: https://docs.google.com/drawings/d/1EK1i4cKLoXrMJmNauKMu3yIO1OULcW0uUXLbo33qoU0/edit?usp=sharing
-      p(
-        """A more detailed syntax illustration would be the image below for graph privileges.
-          |image::grant-privileges-graph.png[title="Syntax of GRANT and DENY Graph Privileges"]""".stripMargin)
+      p("A more detailed syntax illustration would be the image below for graph privileges.")
+      p("image::grant-privileges-graph.png[title=\"Syntax of GRANT and DENY Graph Privileges\"]")
       // image source: https://docs.google.com/drawings/d/1dueKAcaQORul-_Ocb5jK9bUkWgtQfdLdFw4uo7PFjTs/edit
     }
     section("Listing privileges", "administration-security-subgraph-show", "enterprise-edition") {
