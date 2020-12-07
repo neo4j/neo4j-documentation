25ccfea58d2023a8d381dd3a10e3b5f91a45bddb
AlexicaWright <49636617+AlexicaWright@users.noreply.github.com>
2020-12-07 14:11:17 +0100

    4.3 cherry pick 812 (#815)
    * resolved conflicts
    
    * add USE test (#812)
    
    * add USE test
    
    * addressed PR comments

---
+++ cypher/refcard-tests/src/test/scala/org/neo4j/cypher/docgen/refcard/UseTest.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2002-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.cypher.docgen.refcard
+
+import org.neo4j.cypher.docgen.RefcardTest
+import org.neo4j.cypher.docgen.tooling.{DocsExecutionResult, QueryStatisticsTestSupport}
+import org.neo4j.graphdb.Transaction
+
+class UseTest extends RefcardTest with QueryStatisticsTestSupport {
+  val graphDescription = List()
+  val title = "USE"
+  override val linkId = "clauses/use"
+
+  override def assert(tx:Transaction, name: String, result: DocsExecutionResult): Unit = {}
+
+  def text = """
+###dontrun
+// Can't be run since the USE clause is not available in embedded or http sessions.
+
+USE myDatabase
+###
+
+Select `myDatabase` to execute query, or query part, against.
+
+###dontrun
+// Can't be run since the USE clause is not available in embedded or http sessions.
+
+USE neo4j
+MATCH (n:Person)-[:KNOWS]->(m:Person)
+WHERE n.name = 'Alice'
+
+RETURN n, m###
+
+`MATCH` query executed against `neo4j` database.
+
+"""
+}
