221f50d86a801e3587c66d8c1d8766cdd70e5dd5
Martin <martin.bendsoe@neotechnology.com>
2021-03-02 15:25:24 +0100

    4.1 cypher query syntax fix part 11 (#976) (#978)
    Fixed ordering link name, because it is included it must be explicit set.
    Fixed style for order by clause.
    Fixed cypher style for skip clause.
    Updated limit cypher clause.

--- cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/LimitTest.scala
+++ cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/LimitTest.scala
@@ -26,53 +26,47 @@ class LimitTest extends DocumentingTest {
 
   override def doc = new DocBuilder {
     doc("LIMIT", "query-limit")
-    initQueries(
-      """CREATE (a {name: 'A'}),
-                (b {name: 'B'}),
-                (c {name: 'C'}),
-                (d {name: 'D'}),
-                (e {name: 'E'}),
-
-                (a)-[:KNOWS]->(b),
-                (a)-[:KNOWS]->(c),
-                (a)-[:KNOWS]->(d),
-                (a)-[:KNOWS]->(e)
-      """.stripMargin)
-    synopsis("`LIMIT` constrains the number of rows in the output.")
-    p(
-      """* <<limit-introduction, Introduction>>
-        |* <<limit-subset-rows, Return a subset of the rows>>
-        |* <<limit-subset-rows-using-expression, Using an expression with `LIMIT` to return a subset of the rows>>
-      """.stripMargin)
+    initQueries("""CREATE
+                  #  (a {name: 'A'}),
+                  #  (b {name: 'B'}),
+                  #  (c {name: 'C'}),
+                  #  (d {name: 'D'}),
+                  #  (e {name: 'E'}),
+                  #  (a)-[:KNOWS]->(b),
+                  #  (a)-[:KNOWS]->(c),
+                  #  (a)-[:KNOWS]->(d),
+                  #  (a)-[:KNOWS]->(e)""".stripMargin('#'))
+    synopsis("`LIMIT` constrains the number of returned rows.")
+    p("""* <<limit-introduction, Introduction>>
+        #* <<limit-subset-rows, Return a subset of the rows>>
+        #* <<limit-subset-rows-using-expression, Using an expression with `LIMIT` to return a subset of the rows>>""".stripMargin('#'))
     section("Introduction", "limit-introduction") {
       p("""`LIMIT` accepts any expression that evaluates to a positive integer -- however the expression cannot refer to nodes or relationships.""")
       graphViz()
     }
-    section("Return a subset of the rows", "limit-subset-rows") {
-      p(
-        """To return a subset of the result, starting from the top, use this syntax:""".stripMargin)
-      query(
-        """MATCH (n)
-          |RETURN n.name
-          |ORDER BY n.name
-          |LIMIT 3""".stripMargin, ResultAssertions((r) => {
+    section("Return a limited subset of the rows", "limit-subset-rows") {
+      p("""To return a limited subset of the rows, use this syntax:""")
+      query("""MATCH (n)
+              #RETURN n.name
+              #ORDER BY n.name
+              #LIMIT 3""".stripMargin('#'),
+      ResultAssertions((r) => {
         r.toList should equal(List(Map("n.name" -> "A"), Map("n.name" -> "B"), Map("n.name" -> "C")))
       })) {
-        p("The top three items are returned by the example query.")
+        p("Limit to 3 rows by the example query.")
         resultTable()
       }
     }
     section("Using an expression with `LIMIT` to return a subset of the rows", "limit-subset-rows-using-expression") {
-      p(
-        """Limit accepts any expression that evaluates to a positive integer as long as it is not referring to any external variables:""".stripMargin)
-      query(
-        """MATCH (n)
-          |RETURN n.name
-          |ORDER BY n.name
-          |LIMIT toInteger(3 * rand()) + 1""".stripMargin, ResultAssertions((r) => {
+      p("""Limit accepts any expression that evaluates to a positive integer as long as it is not referring to any external variables:""")
+      query("""MATCH (n)
+              #RETURN n.name
+              #ORDER BY n.name
+              #LIMIT 1 + toInteger(3 * rand())""".stripMargin('#'),
+      ResultAssertions((r) => {
           r.toSet should contain(Map("n.name" -> "A"))
         })) {
-        p("Returns one to three top items.")
+        p("Limit 1 row plus randomly 0, 1, or 2. So randomly limit to 1, 2, or 3 rows.")
         resultTable()
       }
     }
