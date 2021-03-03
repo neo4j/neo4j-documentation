9fa5125a4d80a65c270f856a951a0368e643e703
Martin <martin.bendsoe@neotechnology.com>
2021-03-03 10:57:20 +0100

    4.0 cypher query syntax fix part 13 (#985)
    Fixed cypher syntax for delete clause.
    Fixed cypher style for set clause.

--- cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/DeleteTest.scala
+++ cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/DeleteTest.scala
@@ -27,72 +27,67 @@ class DeleteTest extends DocumentingTest with QueryStatisticsTestSupport {
 
   override def doc = new DocBuilder {
     doc("DELETE", "query-delete")
-    initQueries("""CREATE (a:Person {name: 'Andy', age: 36}),
-                  |       (p:Person {name: 'Timothy', age: 25}),
-                  |       (t:Person {name: 'Peter', age: 34}),
-                  |       (z:Person {name: 'UNKNOWN'}),
-                  |       (a)-[:KNOWS]->(t),
-                  |       (a)-[:KNOWS]->(p)""")
+    initQueries("""CREATE
+                  #  (a:Person {name: 'Andy', age: 36}),
+                  #  (p:Person {name: 'Timothy', age: 25}),
+                  #  (t:Person {name: 'Peter', age: 34}),
+                  #  (z:Person {name: 'UNKNOWN'}),
+                  #  (a)-[:KNOWS]->(t),
+                  #  (a)-[:KNOWS]->(p)""".stripMargin('#'))
     synopsis("The `DELETE` clause is used to delete nodes, relationships or paths.")
-    p(
-      """* <<query-delete-introduction, Introduction>>
-        |* <<delete-delete-single-node, Delete a single node>>
-        |* <<delete-delete-all-nodes-and-relationships, Delete all nodes and relationships>>
-        |* <<delete-delete-a-node-with-all-its-relationships, Delete a node with all its relationships>>
-        |* <<delete-delete-relationships-only, Delete relationships only>>""".stripMargin)
+    p("""* <<query-delete-introduction, Introduction>>
+        #* <<delete-delete-single-node, Delete a single node>>
+        #* <<delete-delete-all-nodes-and-relationships, Delete all nodes and relationships>>
+        #* <<delete-delete-a-node-with-all-its-relationships, Delete a node with all its relationships>>
+        #* <<delete-delete-relationships-only, Delete relationships only>>""".stripMargin('#'))
     section("Introduction", "query-delete-introduction") {
-      p(
-        """For removing properties and labels, see <<query-remove>>.
-          |Remember that you cannot delete a node without also deleting relationships that start or end on said node.
-          |Either explicitly delete the relationships, or use `DETACH DELETE`.""".stripMargin)
-      p("""The examples start out with the following database:""".stripMargin)
+      p("""For removing properties and labels, see <<query-remove>>.
+          #Remember that you cannot delete a node without also deleting relationships that start or end on said node.
+          #Either explicitly delete the relationships, or use `DETACH DELETE`.""".stripMargin('#'))
+      p("The examples start out with the following database:")
       graphViz()
     }
     section("Delete single node", "delete-delete-single-node") {
-      p(
-        """To delete a node, use the `DELETE` clause.""".stripMargin)
-      query(
-        """MATCH (n:Person {name: 'UNKNOWN'})
-          |DELETE n""".stripMargin, ResultAssertions((r) => {
+      p("To delete a node, use the `DELETE` clause.")
+      query("""MATCH (n:Person {name: 'UNKNOWN'})
+              #DELETE n""".stripMargin('#'),
+      ResultAssertions((r) => {
           assertStats(r, nodesDeleted = 1)
         })) {
         resultTable()
       }
     }
     section("Delete all nodes and relationships", "delete-delete-all-nodes-and-relationships") {
-      p(
-        """This query isn't for deleting large amounts of data, but is useful when experimenting with small example data sets.""".stripMargin)
-      query(
-        """MATCH (n)
-          |DETACH DELETE n""".stripMargin, ResultAssertions((r) => {
+      p("This query is not for deleting large amounts of data, but is useful when experimenting with small example data sets.")
+      query("""MATCH (n)
+              #DETACH DELETE n""".stripMargin('#'),
+      ResultAssertions((r) => {
           assertStats(r, relationshipsDeleted = 2, nodesDeleted = 4)
         })) {
         resultTable()
       }
     }
     section("Delete a node with all its relationships", "delete-delete-a-node-with-all-its-relationships") {
-      p(
-        """When you want to delete a node and any relationship going to or from it, use `DETACH DELETE`.""".stripMargin)
-      query(
-        """MATCH (n {name: 'Andy'})
-          |DETACH DELETE n""".stripMargin, ResultAssertions((r) => {
+      p("When you want to delete a node and any relationship going to or from it, use `DETACH DELETE`.")
+      query("""MATCH (n {name: 'Andy'})
+              #DETACH DELETE n""".stripMargin('#'),
+      ResultAssertions((r) => {
           assertStats(r, relationshipsDeleted = 2, nodesDeleted = 1)
         })) {
         resultTable()
       }
       note {
-        p( """For `DETACH DELETE` for users with restricted security privileges, see <<operations-manual#detach-delete-restricted-user, Operations Manual -> Fine-grained access control>>.""".stripMargin)
+        p("For `DETACH DELETE` for users with restricted security privileges, see <<operations-manual#detach-delete-restricted-user, Operations Manual -> Fine-grained access control>>.")
       }
     }
     section("Delete relationships only", "delete-delete-relationships-only") {
-      p(
-        """It is also possible to delete relationships only, leaving the node(s) otherwise unaffected.""".stripMargin)
-      query(
-        """MATCH (n {name: 'Andy'})-[r:KNOWS]->()
-          |DELETE r""".stripMargin, ResultAssertions((r) => {
+      p("It is also possible to delete relationships only, leaving the node(s) otherwise unaffected.")
+      query("""MATCH (n {name: 'Andy'})-[r:KNOWS]->()
+              #DELETE r""".stripMargin('#'),
+      ResultAssertions((r) => {
           assertStats(r, relationshipsDeleted = 2, nodesDeleted = 0)
         })) {
-        p("""This deletes all outgoing `KNOWS` relationships from the node with the name *'Andy'*.""".stripMargin)
+        p("""This deletes all outgoing `KNOWS` relationships from the node with the name *'Andy'*.""")
         resultTable()
       }
     }
