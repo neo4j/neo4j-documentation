078b9e74190b487e0c95f30930c1b4c9bece554b
AlexicaWright <49636617+AlexicaWright@users.noreply.github.com>
2021-03-05 17:02:42 +0100

    added admonition about restrictions to SHOW  DBs (#994) (#1003)
    * added admonition about restrictions to SHOW  DBs
    
    * addressed PR comments

--- cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/DatabasesTest.scala
+++ cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/DatabasesTest.scala
@@ -60,6 +60,13 @@ class DatabasesTest extends DocumentingTest with QueryStatisticsTestSupport {
       query("SHOW DATABASES", assertDatabasesShown) {
         resultTable()
       }
+      note {
+        p(
+          """Note that the results of this command are filtered according to the `ACCESS` privileges the user has.
+            |However, a user with `CREATE/DROP DATABASE` or `DATABASE MANAGEMENT` privileges can see all databases regardless of their `ACCESS` privileges.
+            |If a user has not been granted `ACCESS` privilege to any databases, the command can still be executed but will only return the `system` database, which is always visible.
+            |""".stripMargin)
+          }
       p("A particular database can be seen using the command `SHOW DATABASE name`.")
       query("SHOW DATABASE system", assertDatabaseShown("system")) {
         resultTable()
