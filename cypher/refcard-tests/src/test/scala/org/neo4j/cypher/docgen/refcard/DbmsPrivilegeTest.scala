3bd364ddd088816883cae23ae07b1381895a7f9f
Louise Söderström <louise.matematik@gmail.com>
2021-12-21 13:04:58 +0100

    Add impersonation privilege to refcard

--- cypher/refcard-tests/src/test/scala/org/neo4j/cypher/docgen/refcard/DbmsPrivilegeTest.scala
+++ cypher/refcard-tests/src/test/scala/org/neo4j/cypher/docgen/refcard/DbmsPrivilegeTest.scala
@@ -21,7 +21,7 @@ package org.neo4j.cypher.docgen.refcard
 
 class DbmsPrivilegeTest extends AdministrationCommandTestBase {
   val title = "(★) DBMS privileges"
-  override val linkId = "administration/security/administration/#administration-security-administration-dbms-privileges-all"
+  override val linkId = "administration/security/administration/#administration-security-administration-dbms-privileges"
 
   private def setup() = graph.withTx { tx =>
     tx.execute("CREATE ROLE my_role")
@@ -36,7 +36,15 @@ class DbmsPrivilegeTest extends AdministrationCommandTestBase {
 GRANT ALL ON DBMS TO my_role
 ###
 
-Grant privilege to perform all role management, user management, database management and privilege management to a role.
+Grant privilege to perform all role, user, database, privilege, procedure, function and impersonation management to a role.
+
+###assertion=update-one
+//
+
+DENY IMPERSONATE (alice) TO my_role
+###
+
+Deny privilege to impersonate the specified user to a role.
 
 """
   }
