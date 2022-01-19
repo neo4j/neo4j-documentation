04fb63f404a746dbbec2ad9d701af66d21917b08
AlexicaWright <49636617+AlexicaWright@users.noreply.github.com>
2022-01-19 13:19:23 +0100

    fix result assertions

--- cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/OrderByTest.scala
+++ cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/OrderByTest.scala
@@ -96,7 +96,7 @@ class OrderByTest extends DocumentingTest {
               #RETURN n.name, n.age
               #ORDER BY id(n)""".stripMargin('#'),
       ResultAssertions((r) => {
-        r.toList should equal(List(Map("n.name" -> "A", "n.age" -> 34), Map("n.name" -> "B", "n.age" -> 34), Map("n.name" -> "C", "n.age" -> 32)))
+        r.toList should equal(List(Map("n.name" -> "A", "n.age" -> 34), Map("n.name" -> "B", "n.age" -> 36), Map("n.name" -> "C", "n.age" -> 32)))
       })) {
         p("The nodes are returned, sorted by their internal id.")
         resultTable()
@@ -115,7 +115,7 @@ class OrderByTest extends DocumentingTest {
               #RETURN n.name, n.age, n.length
               #ORDER BY keys(n)""".stripMargin('#'),
       ResultAssertions((r) => {
-        r.toList should equal(List(Map("n.length" -> 170, "n.name" -> "A", "n.age" -> 34), Map("n.length" -> 185, "n.name" -> "C", "n.age" -> 32), Map("n.length" -> null, "n.name" -> "B", "n.age" -> 34)))
+        r.toList should equal(List(Map("n.length" -> 170, "n.name" -> "A", "n.age" -> 34), Map("n.length" -> 185, "n.name" -> "C", "n.age" -> 32), Map("n.length" -> null, "n.name" -> "B", "n.age" -> 36)))
       })) {
         p("The nodes are returned, sorted by their properties.")
         resultTable()
