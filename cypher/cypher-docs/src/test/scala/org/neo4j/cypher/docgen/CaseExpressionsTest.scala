6e8f795ba961a7fe43e155528c1ea1538549ac0b
Reneta Popova <reneta.popova@neo4j.com>
2022-02-08 11:33:31 +0000

    Update cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/CaseExpressionsTest.scala
    Co-authored-by: Arne Fischereit <79841228+arnefischereit@users.noreply.github.com>

--- cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/CaseExpressionsTest.scala
+++ cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/CaseExpressionsTest.scala
@@ -133,7 +133,7 @@ class CaseExpressionsTest extends DocumentingTest with QueryStatisticsTestSuppor
     }
     section("Using the result of `CASE` in the succeeding clause or statement", "syntax-use-case-result") {
       p("""You can use the result of `CASE` to set a property(s) or a label(s) on a node or relationship.
-       #For example, instead of specifying the node directly, you can set a property for a node selected by an expression:""".stripMargin('#'))
+          #For example, instead of specifying the node directly, you can set a property for a node selected by an expression:""".stripMargin('#'))
       query("""MATCH (n)
               #WITH n,
               #CASE n.eyes
