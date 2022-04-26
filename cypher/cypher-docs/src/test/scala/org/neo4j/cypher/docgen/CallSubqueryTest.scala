30991630d24b5f8984caebdfacc80ae99c2bc883
Reneta Popova <reneta.popova@neo4j.com>
2022-04-26 09:30:39 +0100

    Update cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/CallSubqueryTest.scala

--- cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/CallSubqueryTest.scala
+++ cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/CallSubqueryTest.scala
@@ -284,7 +284,6 @@ class CallSubqueryTest extends DocumentingTest {
         ) { resultTable() }
         note{
           p("""Up to a point, using a larger batch size will be more performant. The batch size of `2 ROWS` is an example given the small data set used here. For larger data sets you might want to use larger batch sizes, like `10000 ROWS`.""".stripMargin('#'))
-          #However, if you have a larger data set, you might want to use `IN TRANSACTIONS OF 10000 ROWS`.""".stripMargin('#'))
     }
       }
       section("Errors") {
