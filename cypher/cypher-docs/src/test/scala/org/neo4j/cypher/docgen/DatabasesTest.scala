2278793a3a94d47498ea8781d41912931e93b4df
Therese Magnusson <therese.magnusson@neotechnology.com>
2022-10-13 15:12:52 +0200

    Add link to composite databases introduction section (#1632)

--- cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/DatabasesTest.scala
+++ cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/DatabasesTest.scala
@@ -159,9 +159,9 @@ class DatabasesTest extends DocumentingTest with QueryStatisticsTestSupport {
         }
       }
       section("Creating composite databases", "administration-databases-create-composite-database", "enterprise-edition") {
-        // TODO: add a link to the place explaining what composite databases are
         p(
-          """Composite databases do not contain data, but they reference to other databases that can be queried together through their constituent aliases.""".stripMargin)
+          """Composite databases do not contain data, but they reference to other databases that can be queried together through their constituent aliases.
+            |For more information about composite databases, see <<operations-manual#composite-databases-introduction, Operations Manual -> Composite database introduction>>.""".stripMargin)
         p("Composite databases can be created using `CREATE COMPOSITE DATABASE`.")
         query("CREATE COMPOSITE DATABASE inventory", ResultAssertions(r => {
           assertStats(r, systemUpdates = 1)
